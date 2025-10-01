946cc78dd18915c22665d6caffd00b66647c335a
Murat Komurcu <muratkomurrcu@gmail.com>
2025-10-01 15:08:29 +0300

    inference.py

--- starcoder2-stm32-github/inference.py
+++ starcoder2-stm32-github/inference.py
@@ -1,5 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = 'YOUR_USERNAME/starcoder2-stm32'
+model_name = 'MuratKomurcu/starcoder2-stm32'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
